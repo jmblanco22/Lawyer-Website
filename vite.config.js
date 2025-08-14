448cfc5c8af6209a95db5582ffe3a767f0e55f75
José Blanco <Jose Blanco>
2025-08-14 13:35:11 -0500

    build: docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If your Pages URL is https://USERNAME.github.io/law-office-react-refined/
-const base = '/law-office-react-refined/'   // ← change if your repo name is different
-// If your URL is https://USERNAME.github.io/ (user site), use base = '/'
-
 export default defineConfig({
   plugins: [react()],
-  base,
-  build: { outDir: 'docs' }   // build straight into /docs
+  base: '/law-office-react-refined/',   // EXACT repo name + leading/trailing slashes
+  build: { outDir: 'docs' }
 })

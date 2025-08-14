ea6a1cac6023fcb3fde505d4e60c5c3190879b23
José Blanco <Jose Blanco>
2025-08-14 13:54:57 -0500

    new deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/law-office-react-refined/',   // EXACT repo name + leading/trailing slashes
+  base: './',            // <-- relative
   build: { outDir: 'docs' }
 })

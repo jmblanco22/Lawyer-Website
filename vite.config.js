5f246135c1f0a088925e7efb9f2f6c85cda142ac
José Blanco <Jose Blanco>
2025-08-14 11:45:44 -0500

    Build to docs for Gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// If your site is https://USERNAME.github.io/REPO_NAME/ set base to "/REPO_NAME/"
+// If your site is https://USERNAME.github.io/ set base to "/"
+const base = '/REPO_NAME/'   // <-- change to your repo name, or "/" for user/org site
+
 export default defineConfig({
-  base: '/Lawyer-Website/',
   plugins: [react()],
+  base,
+  build: { outDir: 'docs' }  // build straight into /docs
 })

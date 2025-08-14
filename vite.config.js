8b7010e64e7c9ffc4781afa068785ade8dd90a72
José Blanco <Jose Blanco>
2025-08-14 14:06:38 -0500

    new index html fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',            // <-- relative
+  base: './',             // relative base avoids repo-name path issues
   build: { outDir: 'docs' }
 })

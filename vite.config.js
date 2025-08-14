eede4b33a96eb16602d3e6e01b7aedffda1c278e
José Blanco <Jose Blanco>
2025-08-14 11:57:12 -0500

    new build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If your site is https://USERNAME.github.io/REPO_NAME/ set base to "/REPO_NAME/"
-// If your site is https://USERNAME.github.io/ set base to "/"
-const base = '/REPO_NAME/'   // <-- change to your repo name, or "/" for user/org site
+// If your Pages URL is https://USERNAME.github.io/law-office-react-refined/
+const base = '/law-office-react-refined/'   // ← change if your repo name is different
+// If your URL is https://USERNAME.github.io/ (user site), use base = '/'
 
 export default defineConfig({
   plugins: [react()],
   base,
-  build: { outDir: 'docs' }  // build straight into /docs
+  build: { outDir: 'docs' }   // build straight into /docs
 })
